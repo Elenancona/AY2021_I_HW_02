eb66353b5addf832d6d63c9da62a3054b5f7453a
EleAnc <elena.ancona@mail.polimi.it>
2020-10-12 23:21:56 +0200

    Final project?

--- ANCONA_ELENA.cydsn/InterruptRoutines.c
+++ ANCONA_ELENA.cydsn/InterruptRoutines.c
@@ -14,96 +14,95 @@
 #include "InterruptRoutines.h"
 #include "isr_1.h"
 #include "project.h"
-#include "PWM_1.h"
+#include "PWM_RED_LED.h"
+#include "PWM_GREEN_LED.h" 
+
 
 #define DUE_SEC 255
 #define UN_SEC 127
 #define MEZZO_SEC 63
-#define QUARTO_SEC 32
 
 
-int counter=0;
+int counter=1;
+
 
 
 CY_ISR(Custom_LED_ISR)
 
-{    
+{
+      
   counter ++;
     
     switch(counter)
     {
         
         case(1):
-        PWM_1_SetCompareMode1(2);
-        PWM_1_SetCompareMode2(2);
-        PWM_1_WritePeriod(DUE_SEC);
-        PWM_1_WriteCompare1(0);
-        PWM_1_WriteCompare2(0);
+        PWM_GREEN_LED_SetCompareMode(2); //CMP type is less than or equal
+        PWM_RED_LED_SetCompareMode(2);
+        PWM_GREEN_LED_WritePeriod(DUE_SEC);
+        PWM_RED_LED_WritePeriod(DUE_SEC);
+        PWM_GREEN_LED_WriteCompare(0); // LED is always on 
+        PWM_RED_LED_WriteCompare(0);
         break;
         
         case(2):
-        PWM_1_SetCompareMode1(2);
-        PWM_1_SetCompareMode2(2);
-        PWM_1_WritePeriod(DUE_SEC);
-        PWM_1_WriteCompare1(128);
-        PWM_1_WriteCompare2(0);
+        PWM_GREEN_LED_SetCompareMode(2);
+        PWM_RED_LED_SetCompareMode(2);
+        PWM_GREEN_LED_WritePeriod(DUE_SEC);
+        PWM_RED_LED_WritePeriod(DUE_SEC);
+        PWM_GREEN_LED_WriteCompare(128);
+        PWM_RED_LED_WriteCompare(0);        
         break;
         
         case(3):
-        PWM_1_SetCompareMode1(2);
-        PWM_1_SetCompareMode2(3);
-        PWM_1_WritePeriod(DUE_SEC);
-        PWM_1_WriteCompare1(0);
-        PWM_1_WriteCompare2(128);
+        PWM_GREEN_LED_SetCompareMode(2);
+        PWM_RED_LED_SetCompareMode(4); //CMP type is grater than or equal
+        PWM_GREEN_LED_WritePeriod(DUE_SEC);
+        PWM_RED_LED_WritePeriod(DUE_SEC);
+        PWM_GREEN_LED_WriteCompare(0);
+        PWM_RED_LED_WriteCompare(128);        
         break;
         
         case(4):
-        PWM_1_SetCompareMode1(3);
-        PWM_1_SetCompareMode2(2);
-        PWM_1_WritePeriod(UN_SEC);
-        PWM_1_WriteCompare1(64);
-        PWM_1_WriteCompare2(64);
+        PWM_GREEN_LED_SetCompareMode(4);
+        PWM_RED_LED_SetCompareMode(2);
+        PWM_GREEN_LED_WritePeriod(UN_SEC);
+        PWM_RED_LED_WritePeriod(UN_SEC);
+        PWM_GREEN_LED_WriteCompare(64);
+        PWM_RED_LED_WriteCompare(64);
         break;
         
         case(5):
-        PWM_1_SetCompareMode1(2);
-        PWM_1_SetCompareMode2(3);
-        PWM_1_WritePeriod(MEZZO_SEC);
-        PWM_1_WriteCompare1(32);
-        PWM_1_WriteCompare2(32);
+        PWM_GREEN_LED_SetCompareMode(2);
+        PWM_RED_LED_SetCompareMode(4);
+        PWM_GREEN_LED_WritePeriod(MEZZO_SEC);
+        PWM_RED_LED_WritePeriod(MEZZO_SEC);
+        PWM_GREEN_LED_WriteCompare(32);
+        PWM_RED_LED_WriteCompare(32);
         break;
         
         case(6):
-        PWM_1_SetCompareMode1(2);
-        PWM_1_SetCompareMode2(2);
-        PWM_1_WritePeriod(DUE_SEC);
-        PWM_1_WriteCompare1(128);
-        PWM_1_WriteCompare2(64);
+        PWM_GREEN_LED_SetCompareMode(2);
+        PWM_RED_LED_SetCompareMode(2);
+        PWM_GREEN_LED_WritePeriod(DUE_SEC);
+        PWM_RED_LED_WritePeriod(DUE_SEC);
+        PWM_GREEN_LED_WriteCompare(128);
+        PWM_RED_LED_WriteCompare(64);
         break;
         
         case(7):
-        PWM_1_SetCompareMode1(2);
-        PWM_1_SetCompareMode2(3);
-        PWM_1_WritePeriod(UN_SEC);
-        PWM_1_WriteCompare1(192);
-        PWM_1_WriteCompare2(128);
-       
+        PWM_GREEN_LED_SetCompareMode(2);
+        PWM_RED_LED_SetCompareMode(3);
+        PWM_GREEN_LED_WritePeriod(UN_SEC);
+        PWM_RED_LED_WritePeriod(DUE_SEC);
+        PWM_GREEN_LED_WriteCompare(64);
+        PWM_RED_LED_WriteCompare(128);           
         
-        counter=0;
+        counter=0; //Counter 0 so the next time I push it I will be in case 1
         break;
     }
   
    
 }
-    
-
-
-
-    
-    
-// Toggle The LED
-//Red_LED_Write(Green_LED_Read());
-//Green_LED_Write(~Green_LED_Read());
-//}
 
 /* [] END OF FILE */
--- ANCONA_ELENA.cydsn/main.c
+++ ANCONA_ELENA.cydsn/main.c
@@ -22,11 +22,22 @@ int main(void)
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     
+
     
-    PWM_1_Start();
+    PWM_RED_LED_Start();
+    PWM_GREEN_LED_Start();
+      
     
     Button_isr_1_StartEx(Custom_LED_ISR);
     
+    // To have the system at state 1 at power-on 
+    
+        PWM_GREEN_LED_SetCompareMode(2); 
+        PWM_RED_LED_SetCompareMode(2);
+        PWM_GREEN_LED_WritePeriod(255);
+        PWM_RED_LED_WritePeriod(255);
+        PWM_GREEN_LED_WriteCompare(0); 
+        PWM_RED_LED_WriteCompare(0);
 
     for(;;)
     {
